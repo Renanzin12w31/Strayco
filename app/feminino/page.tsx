c1df375cdf462cbd9e4f9f426bc401f4a033beb8
Renanzin12w31 <85647187+Renanzin12w31@users.noreply.github.com>
2026-02-27 02:08:34 -0300

    Remove salePrice prop from product component
    Removed salePrice prop from product component.

--- app/feminino/page.tsx
+++ app/feminino/page.tsx
@@ -31,7 +31,6 @@ export default function FemininoPage() {
                                 id={product.id}
                                 name={product.name}
                                 price={product.price}
-                                salePrice={product.salePrice || undefined}
                                 image={product.images[0]}
                                 featured={product.featured}
                                 isNew={product.isNew}
